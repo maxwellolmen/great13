bfad05721859001a0cf23f2ca1feced2b39a361d
Maxwell Olmen <maxwell@10-4-234-48.vlan890.client.wireless.uic.edu>
2021-09-23 14:15:35 -0500

    Simply setChar()

--- main.cpp
+++ main.cpp
@@ -47,14 +47,14 @@ short numBoardPieces, moveNumber;
 // Display the game instructions.
 void displayInstructions() {
     cout << "Class: CS 141             \n"
-         << "Program: #2 Great 13      \n"
-         << "  \n"
-         << "Make a series of jumps until there is a single piece left in the   \n"
-         << "middle. On each move you must jump an adjacent piece into an empty \n"
-         << "square, jumping horizontally, vertically, or diagonally.           \n"
-         << "Input of 'R' resets the board back to the beginning, and input of  \n"
-         << "'X' exits the game.'   \n"
-         << endl;
+    << "Program: #2 Great 13      \n"
+    << "  \n"
+    << "Make a series of jumps until there is a single piece left in the   \n"
+    << "middle. On each move you must jump an adjacent piece into an empty \n"
+    << "square, jumping horizontally, vertically, or diagonally.           \n"
+    << "Input of 'R' resets the board back to the beginning, and input of  \n"
+    << "'X' exits the game.'   \n"
+    << endl;
 } //end displayInstructions()
 
 // Set up the board, including display characters and totals.
@@ -69,13 +69,13 @@ void setupBoard() {
 // Display the board, using the global variables pA..pM
 void displayBoard() {
     cout << "\n "
-         << "    Board   " << " Position \n"
-         << "       " << pA << "          A    \n"
-         << "     " << pB << " " << pC << " " << pD << "      B C D  \n"
-         << "   " << pE << " " << pF << " " << pG << " " << pH << " " << pI << "  E F G H I\n"
-         << "     " << pJ << " " << pK << " " << pL << "      J K L  \n"
-         << "       " << pM << "          M    \n"
-         << endl;
+    << "    Board   " << " Position \n"
+    << "       " << pA << "          A    \n"
+    << "     " << pB << " " << pC << " " << pD << "      B C D  \n"
+    << "   " << pE << " " << pF << " " << pG << " " << pH << " " << pI << "  E F G H I\n"
+    << "     " << pJ << " " << pK << " " << pL << "      J K L  \n"
+    << "       " << pM << "          M    \n"
+    << endl;
 } //end displayBoard()
 
 // Checks and returns whether the current requested move follows a proper line
@@ -84,65 +84,65 @@ bool isAdjacent() {
     // After checking first character, every possible line combination for the second and third are checked
     if (moveFrom == 'A') {
         if ((moveJump == 'B' && moveTo == 'E')
-            || (moveJump == 'C' && moveTo == 'G')
-            || (moveJump == 'D' && moveTo == 'I')) {
+        || (moveJump == 'C' && moveTo == 'G')
+        || (moveJump == 'D' && moveTo == 'I')) {
             return true;
         }
     } else if (moveFrom == 'B') {
         if ((moveJump == 'C' && moveTo == 'D')
-            || (moveJump == 'F' && moveTo == 'J')
-            || (moveJump == 'G' && moveTo == 'L')) {
+        || (moveJump == 'F' && moveTo == 'J')
+        || (moveJump == 'G' && moveTo == 'L')) {
             return true;
         }
     } else if (moveFrom == 'C' && moveJump == 'G' && moveTo == 'K') {
         return true;
     } else if (moveFrom == 'D') {
         if ((moveJump == 'C' && moveTo == 'B')
-            || (moveJump == 'G' && moveTo == 'J')
-            || (moveJump == 'H' && moveTo == 'L')) {
+        || (moveJump == 'G' && moveTo == 'J')
+        || (moveJump == 'H' && moveTo == 'L')) {
             return true;
         }
     } else if (moveFrom == 'E') {
         if ((moveJump == 'B' && moveTo == 'A')
-            || (moveJump == 'F' && moveTo == 'G')
-            || (moveJump == 'J' && moveTo == 'M')) {
+        || (moveJump == 'F' && moveTo == 'G')
+        || (moveJump == 'J' && moveTo == 'M')) {
             return true;
         }
     } else if (moveFrom == 'F' && moveJump == 'G' && moveTo == 'H') {
         return true;
     } else if (moveFrom == 'G') {
         if ((moveJump == 'C' && moveTo == 'A')
-            || (moveJump == 'F' && moveTo == 'E')
-            || (moveJump == 'H' && moveTo == 'I')
-            || (moveJump == 'K' && moveTo == 'M')) {
+        || (moveJump == 'F' && moveTo == 'E')
+        || (moveJump == 'H' && moveTo == 'I')
+        || (moveJump == 'K' && moveTo == 'M')) {
             return true;
         }
     } else if (moveFrom == 'H' && moveJump == 'G' && moveTo == 'F') {
         return true;
     } else if (moveFrom == 'I') {
         if ((moveJump == 'D' && moveTo == 'A')
-            || (moveJump == 'H' && moveTo == 'G')
-            || (moveJump == 'L' && moveTo == 'M')) {
+        || (moveJump == 'H' && moveTo == 'G')
+        || (moveJump == 'L' && moveTo == 'M')) {
             return true;
         }
     } else if (moveFrom == 'J') {
         if ((moveJump == 'F' && moveTo == 'B')
-            || (moveJump == 'G' && moveTo == 'D')
-            || (moveJump == 'K' && moveTo == 'L')) {
+        || (moveJump == 'G' && moveTo == 'D')
+        || (moveJump == 'K' && moveTo == 'L')) {
             return true;
         }
     } else if (moveFrom == 'K' && moveJump == 'G' && moveTo == 'C') {
         return true;
     } else if (moveFrom == 'L') {
         if ((moveJump == 'H' && moveTo == 'D')
-            || (moveJump == 'G' && moveTo == 'B')
-            || (moveJump == 'K' && moveTo == 'J')) {
+        || (moveJump == 'G' && moveTo == 'B')
+        || (moveJump == 'K' && moveTo == 'J')) {
             return true;
         }
     } else if (moveFrom == 'M') {
         if ((moveJump == 'J' && moveTo == 'E')
-            || (moveJump == 'K' && moveTo == 'G')
-            || (moveJump == 'L' && moveTo == 'I')) {
+        || (moveJump == 'K' && moveTo == 'G')
+        || (moveJump == 'L' && moveTo == 'I')) {
             return true;
         }
     }
@@ -151,89 +151,80 @@ bool isAdjacent() {
 }
 
 // Gets the character (either # or .) at the position specified by letter-code
-char getChar(char position) {
-    // Switch letter-code position to match to existing char variable, then return
+char& getChar(char position) {
+    // Switch letter-code position to match to existing char variable, then return variable as reference
     switch (position) {
         case 'A':
             return pA;
-        case 'B':
-            return pB;
-        case 'C':
-            return pC;
-        case 'D':
-            return pD;
-        case 'E':
-            return pE;
-        case 'F':
-            return pF;
-        case 'G':
-            return pG;
-        case 'H':
-            return pH;
-        case 'I':
-            return pI;
-        case 'J':
-            return pJ;
-        case 'K':
-            return pK;
-        case 'L':
-            return pL;
-        case 'M':
-            return pM;
+            case 'B':
+                return pB;
+                case 'C':
+                    return pC;
+                    case 'D':
+                        return pD;
+                        case 'E':
+                            return pE;
+                            case 'F':
+                                return pF;
+                                case 'G':
+                                    return pG;
+                                    case 'H':
+                                        return pH;
+                                        case 'I':
+                                            return pI;
+                                            case 'J':
+                                                return pJ;
+                                                case 'K':
+                                                    return pK;
+                                                    case 'L':
+                                                        return pL;
+                                                        case 'M':
+                                                            return pM;
     }
 
-    return ' ';
+    return pA;
 }
 
 // Sets character (normally with either # or .) to the letter-code position specified
 void setChar(char position, char c) {
-    // Switch letter-code position to match to existing char variable, then assign
-    switch (position) {
-        case 'A':
-            pA = c;
-            break;
-        case 'B':
-            pB = c;
-            break;
-        case 'C':
-            pC = c;
-            break;
-        case 'D':
-            pD = c;
-            break;
-        case 'E':
-            pE = c;
-            break;
-        case 'F':
-            pF = c;
-            break;
-        case 'G':
-            pG = c;
-            break;
-        case 'H':
-            pH = c;
-            break;
-        case 'I':
-            pI = c;
-            break;
-        case 'J':
-            pJ = c;
-            break;
-        case 'K':
-            pK = c;
-            break;
-        case 'L':
-            pL = c;
-            break;
-        case 'M':
-            pM = c;
-            break;
-    }
+    getChar(position) = c;
 }
 
 // Checks and returns whether the character at the specified letter-code position contains a piece (#)
 bool hasPiece(char position) {
     return getChar(position) == '#';
+}
+
+bool checkErrors() {
+    // Ensure set of pieces (from, jump, to) are adjacent.  If they aren't, then
+    // continue back up to re-run the while(...) loop.
+    if (!isAdjacent()) {
+        cout << endl << "   *** Pieces must be adjacent. Please retry" << endl << endl;
+        return false;
+    }
+
+    // Ensure location we're moving FROM has a PlayerPiece in it. If it doesn't, then
+    // continue back up to re-run the while(...) loop.
+    if (!hasPiece(moveFrom)) {
+        cout << endl << "   *** Source must have a piece. Please retry" << endl << endl;
+        return false;
+    }
+
+    // Ensure piece being jumped isn't blank. If it is, then
+    // continue back up to re-run the while(...) loop.
+    if (!hasPiece(moveJump)) {
+        cout << endl << "   *** Must jump a piece. Please retry" << endl << endl;
+        return false;
+    }
+
+    // Ensure location we're moving TO is blank. If it is not, then
+    // continue back up to re-run the while(...) loop.
+    if (hasPiece(moveTo)) {
+        cout << endl << "   *** Destination must be blank. Retry" << endl << endl;
+        return false;
+    }
+
+    return true;
 }
 
 // Checks and returns whether the requested move is legal, once known that it is a proper line
@@ -248,16 +239,16 @@ bool isLegal(char moveFrom, char moveJump, char moveTo) {
 bool hasMove() {
     // Check legality of every possible move (manually entered)
     if (isLegal('A', 'B', 'E') || isLegal('A', 'C', 'G') || isLegal('A', 'D', 'I')
-        || isLegal('B', 'C', 'D') || isLegal('B', 'G', 'L') || isLegal('B', 'F', 'J')
-        || isLegal('C', 'G', 'K') || isLegal('D', 'C', 'B') || isLegal('D', 'G', 'J')
-        || isLegal('D', 'H', 'L') || isLegal('E', 'B', 'A') || isLegal('E', 'F', 'G')
-        || isLegal('E', 'J', 'M') || isLegal('F', 'G', 'H') || isLegal('G', 'C', 'A')
-        || isLegal('G', 'F', 'E') || isLegal('G', 'H', 'I') || isLegal('G', 'K', 'M')
-        || isLegal('H', 'G', 'F') || isLegal('I', 'D', 'A') || isLegal('I', 'H', 'G')
-        || isLegal('I', 'L', 'M') || isLegal('J', 'F', 'B') || isLegal('J', 'G', 'D')
-        || isLegal('J', 'K', 'L') || isLegal('K', 'G', 'C') || isLegal('L', 'K', 'J')
-        || isLegal('L', 'G', 'B') || isLegal('L', 'H', 'D') || isLegal('M', 'J', 'E')
-        || isLegal('M', 'K', 'G') || isLegal('M', 'L', 'I')) {
+    || isLegal('B', 'C', 'D') || isLegal('B', 'G', 'L') || isLegal('B', 'F', 'J')
+    || isLegal('C', 'G', 'K') || isLegal('D', 'C', 'B') || isLegal('D', 'G', 'J')
+    || isLegal('D', 'H', 'L') || isLegal('E', 'B', 'A') || isLegal('E', 'F', 'G')
+    || isLegal('E', 'J', 'M') || isLegal('F', 'G', 'H') || isLegal('G', 'C', 'A')
+    || isLegal('G', 'F', 'E') || isLegal('G', 'H', 'I') || isLegal('G', 'K', 'M')
+    || isLegal('H', 'G', 'F') || isLegal('I', 'D', 'A') || isLegal('I', 'H', 'G')
+    || isLegal('I', 'L', 'M') || isLegal('J', 'F', 'B') || isLegal('J', 'G', 'D')
+    || isLegal('J', 'K', 'L') || isLegal('K', 'G', 'C') || isLegal('L', 'K', 'J')
+    || isLegal('L', 'G', 'B') || isLegal('L', 'H', 'D') || isLegal('M', 'J', 'E')
+    || isLegal('M', 'K', 'G') || isLegal('M', 'L', 'I')) {
         return true;
     }
 
@@ -288,10 +279,9 @@ int main() {
         // display the board, and continue back up to re-run the while(...) loop
         if (moveFrom == 'R') {
             cout << endl << "   *** Restarting" << endl;
-
             setupBoard();
-
             displayBoard();
+
             continue;
         }
 
@@ -303,31 +293,7 @@ int main() {
         moveJump = toupper(moveJump);
         moveTo = toupper(moveTo);
 
-        // Ensure set of pieces (from, jump, to) are adjacent.  If they aren't, then
-        // continue back up to re-run the while(...) loop.
-        if (!isAdjacent()) {
-            cout << endl << "   *** Pieces must be adjacent. Please retry" << endl << endl;
-            continue;
-        }
-
-        // Ensure location we're moving FROM has a PlayerPiece in it. If it doesn't, then
-        // continue back up to re-run the while(...) loop.
-        if (!hasPiece(moveFrom)) {
-            cout << endl << "   *** Source must have a piece. Please retry" << endl << endl;
-            continue;
-        }
-
-        // Ensure piece being jumped isn't blank. If it is, then
-        // continue back up to re-run the while(...) loop.
-        if (!hasPiece(moveJump)) {
-            cout << endl << "   *** Must jump a piece. Please retry" << endl << endl;
-            continue;
-        }
-
-        // Ensure location we're moving TO is blank. If it is not, then
-        // continue back up to re-run the while(...) loop.
-        if (hasPiece(moveTo)) {
-            cout << endl << "   *** Destination must be blank. Retry" << endl << endl;
+        if (!checkErrors()) {
             continue;
         }
 
