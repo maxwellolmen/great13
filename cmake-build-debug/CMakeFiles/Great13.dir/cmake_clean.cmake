419adb972e69dea3eb2b0eb67c973b35015b98cb
Maxwell Olmen <maxwell@10-4-234-48.vlan890.client.wireless.uic.edu>
2021-09-23 14:17:17 -0500

    Fixed formatting

---
+++ cmake-build-debug/CMakeFiles/Great13.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Great13.dir/main.cpp.o"
+  "Great13"
+  "Great13.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Great13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Great13.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Great13.
+# This may be replaced when dependencies are built.
